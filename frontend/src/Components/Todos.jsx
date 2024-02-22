7da77320282be6f6af0c42456c8f911e248ad057
Yuho Jeong <yuho8437@gmail.com>
2024-02-22 15:55:45 +0900

    refactor: update codes with axios

--- frontend/src/Components/Todos.jsx
+++ frontend/src/Components/Todos.jsx
@@ -1,46 +1,50 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import {
-    Box,
-    Button,
-    Flex,
-    Input,
-    InputGroup,
-    Modal,
-    ModalBody,
-    ModalCloseButton,
-    ModalContent,
-    ModalFooter,
-    ModalHeader,
-    ModalOverlay,
-    Stack,
-    Text,
-    useDisclosure
+  Box,
+  Button,
+  Flex,
+  Input,
+  InputGroup,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Stack,
+  Text,
+  useDisclosure,
 } from "@chakra-ui/react";
+import axios from "axios";
 
 const TodosContext = React.createContext({
-  todos: [], fetchTodos: () => {}
-})
+  todos: [],
+  fetchTodos: () => {},
+});
 
 function AddTodo() {
-  const [item, setItem] = React.useState("")
-  const {todos, fetchTodos} = React.useContext(TodosContext)
+  const [item, setItem] = React.useState("");
+  const { todos, fetchTodos } = React.useContext(TodosContext);
 
-  const handleInput = event  => {
-    setItem(event.target.value)
-  }
+  const handleInput = (event) => {
+    setItem(event.target.value);
+  };
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
+    event.preventDefault(); // Prevent default form submission behavior
     const newTodo = {
-      "id": todos.length + 1,
-      "item": item
-    }
+      id: todos.length + 1,
+      item: item,
+    };
 
-    fetch("http://localhost:8000/todo", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(newTodo)
-    }).then(fetchTodos)
-  }
+    try {
+      await axios.post("http://localhost:8000/todo", newTodo);
+      fetchTodos(); // Refresh todos list
+    } catch (error) {
+      console.error("There was an error: ", error);
+    }
+  };
 
   return (
     <form onSubmit={handleSubmit}>
@@ -54,32 +58,34 @@ function AddTodo() {
         />
       </InputGroup>
     </form>
-  )
+  );
 }
 
-function UpdateTodo({item, id}) {
-  const {isOpen, onOpen, onClose} = useDisclosure()
-  const [todo, setTodo] = useState(item)
-  const {fetchTodos} = React.useContext(TodosContext)
+function UpdateTodo({ item, id }) {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [todo, setTodo] = useState(item);
+  const { fetchTodos } = React.useContext(TodosContext);
 
   const updateTodo = async () => {
-    await fetch(`http://localhost:8000/todo/${id}`, {
-      method: "PUT",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ item: todo })
-    })
-    onClose()
-    await fetchTodos()
-  }
+    try {
+      await axios.put(`http://localhost:8000/todo/${id}`, { item: todo });
+      onClose(); // Close modal after update
+      fetchTodos(); // Refresh todos list
+    } catch (error) {
+      console.error("There was an error updating the todo", error);
+    }
+  };
 
   return (
     <>
-      <Button h="1.5rem" size="sm" onClick={onOpen}>Update Todo</Button>
+      <Button h="1.5rem" size="sm" onClick={onOpen}>
+        Update Todo
+      </Button>
       <Modal isOpen={isOpen} onClose={onClose}>
-        <ModalOverlay/>
+        <ModalOverlay />
         <ModalContent>
           <ModalHeader>Update Todo</ModalHeader>
-          <ModalCloseButton/>
+          <ModalCloseButton />
           <ModalBody>
             <InputGroup size="md">
               <Input
@@ -88,73 +94,78 @@ function UpdateTodo({item, id}) {
                 placeholder="Add a todo item"
                 aria-label="Add a todo item"
                 value={todo}
-                onChange={e => setTodo(e.target.value)}
+                onChange={(e) => setTodo(e.target.value)}
               />
             </InputGroup>
           </ModalBody>
 
           <ModalFooter>
-            <Button h="1.5rem" size="sm" onClick={updateTodo}>Update Todo</Button>
+            <Button h="1.5rem" size="sm" onClick={updateTodo}>
+              Update Todo
+            </Button>
           </ModalFooter>
         </ModalContent>
       </Modal>
     </>
-  )
+  );
 }
 
-function DeleteTodo({id}) {
-  const {fetchTodos} = React.useContext(TodosContext)
+function DeleteTodo({ id }) {
+  const { fetchTodos } = React.useContext(TodosContext);
 
   const deleteTodo = async () => {
-    await fetch(`http://localhost:8000/todo/${id}`, {
-      method: "DELETE",
-      headers: { "Content-Type": "application/json" },
-      body: { "id": id }
-    })
-    await fetchTodos()
-  }
+    try {
+      await axios.delete(`http://localhost:8000/todo/${id}`);
+      fetchTodos(); // Refresh todos list
+    } catch (error) {
+      console.error("There was an error deleting the todo", error);
+    }
+  };
 
   return (
-    <Button h="1.5rem" size="sm" onClick={deleteTodo}>Delete Todo</Button>
-  )
+    <Button h="1.5rem" size="sm" onClick={deleteTodo}>
+      Delete Todo
+    </Button>
+  );
 }
 
-function TodoHelper({item, id, fetchTodos}) {
+function TodoHelper({ item, id, fetchTodos }) {
   return (
     <Box p={1} shadow="sm">
       <Flex justify="space-between">
         <Text mt={4} as="div">
           {item}
           <Flex align="end">
-            <UpdateTodo item={item} id={id} fetchTodos={fetchTodos}/>
-            <DeleteTodo id={id} fetchTodos={fetchTodos}/>
+            <UpdateTodo item={item} id={id} fetchTodos={fetchTodos} />
+            <DeleteTodo id={id} fetchTodos={fetchTodos} />
           </Flex>
         </Text>
       </Flex>
     </Box>
-  )
+  );
 }
 
 export default function Todos() {
-  const [todos, setTodos] = useState([])
+  const [todos, setTodos] = useState([]);
   const fetchTodos = async () => {
-    const response = await fetch("http://localhost:8000/todo")
-    const todos = await response.json()
-    setTodos(todos.data)
-  }
+    try {
+      const response = await axios.get("http://localhost:8000/todo");
+      setTodos(response.data.data); // Assuming the response has a data object
+    } catch (error) {
+      console.error("There was an error fetching the todos", error);
+    }
+  };
   useEffect(() => {
-    fetchTodos()
-  }, [])
+    fetchTodos();
+  }, []);
   return (
-    <TodosContext.Provider value={{todos, fetchTodos}}>
+    <TodosContext.Provider value={{ todos, fetchTodos }}>
       <AddTodo />
       <Stack spacing={5}>
-        {
-          todos.map((todo) => (
-            <TodoHelper item={todo.item} id={todo.id} fetchTodos={fetchTodos} />
-          ))
-        }
+        {todos.map((todo) => (
+          <TodoHelper item={todo.item} id={todo.id} fetchTodos={fetchTodos} />
+        ))}
       </Stack>
     </TodosContext.Provider>
-  )
+  );
 }
